1d08f02541ab78a06d3f11ff074bb9db87ad90ca
nbaksa3@gmail.com <nbaksa3@gmail.com>
2021-06-28 18:15:56 +0900

    18

---
+++ backend/monaco/common/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+class connection(APIView):
+    def get(self, request):
+        return Response({'connection': 'SUCCESS'})
+# Create your views here.
