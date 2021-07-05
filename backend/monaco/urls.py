42c117551f618a6336a3c0d6912c250729eeac51
nbaksa3@gmail.com <nbaksa3@gmail.com>
2021-07-05 18:01:50 +0900

    main

--- backend/monaco/urls.py
+++ backend/monaco/urls.py
@@ -1,17 +1,3 @@
-"""admin URL Configuration
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from common.views import connection
 from django.conf.urls import include, url
 from django.urls import path
@@ -27,14 +13,13 @@ urlpatterns = [
 ]
 
 '''
-CBV (Class Based View)
-from common.views import Connection
+CBV 방식 (Class Based View)
+from django.conf.urls import url
+from .views import Members as members
+from .views import Member as member
 from django.urls import path, include
-from rest_framework import routers
-# router = routers.DefaultRouter()
 urlpatterns = [
-    path('connection', Connection.as_view()),
-    path('board', include('board.urls')),
-    path('member', include('member.urls')),
+    url('/register', members.as_view()),
+    path('/<int:pk>/', member.as_view()),
 ]
 '''
